34e2f3d013186ee33c20b06d82d582891e17ff13
Plinio <plininhodf@gmail.com>
2023-07-03 22:12:32 -0300

    Iniciando projeto 3-dt-money
    aplicação para ter um controle economico, onde o usuario adiciona seus gastos informando se é uma entrada ou saida, esses valores são listados em uma tabela e o total é sempre atualizado para ter seu controle em dia

---
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -0,0 +1,48 @@
+import { useContext } from 'react'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { SearchForm } from '../Transactions/components/SearchForm'
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactionTable,
+} from './styles'
+import { TransactionsContext } from '../../contexts/TransactionContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+
+export function Transactions() {
+  const { transactions } = useContext(TransactionsContext)
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionContainer>
+        <SearchForm />
+
+        <TransactionTable>
+          <tbody>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}{' '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
+    </div>
+  )
+}
